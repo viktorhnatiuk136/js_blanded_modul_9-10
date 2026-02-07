af62caeff37ac67fc5af09985a97587110d10079
Galyna-kud <galynakud121@gmail.com>
2026-02-07 17:07:45 +0200

    update js

--- src/js/markup-tasks.js
+++ src/js/markup-tasks.js
@@ -1,10 +1,12 @@
 
-
 export function markupItem (obj) {
-    return `<li class="task-list-item">
+    return `<li class="task-list-item" data-id=${obj.id}>
       <button class="task-list-item-btn">Delete</button>
       <h3>${obj.taskName}</h3>
       <p>${obj.taskDescription}</p>
   </li>`
 };
 
+export function markupList(array) { 
+  return array.map(markupItem).join("");
+}
--- src/js/tasks.js
+++ src/js/tasks.js
@@ -1,33 +1,63 @@
+import { nanoid } from 'nanoid'
+
 import refs from "./refs";
-import { markupItem } from "./markup-tasks";
+import { markupItem, markupList } from "./markup-tasks";
 
-refs.form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const taskObj = {
-        taskName: "",
-        taskDescription: ""
+const itemKey = "tasks";
+const taskList = JSON.parse(localStorage.getItem(itemKey));
+
+if (!taskList) {
+    localStorage.setItem(itemKey, JSON.stringify([]));
+} else {
+    refs.list.insertAdjacentHTML("beforeend", markupList(taskList));
+}
+
+refs.form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const taskName = event.target.elements.taskName.value;
+  const taskDescription = event.target.elements.taskDescription.value;
+
+    if (taskName === "" || taskDescription === "") {
+        return alert("Заповніть ці поля!");
+    }
+    
+    const newTask = {
+    id: nanoid(),
+    taskName,
+    taskDescription,
     };
-    taskObj.taskName = event.target.elements.taskName.value;
-    taskObj.taskDescription = event.target.elements.taskDescription.value;
 
-    console.log(taskObj);
+    const taskList = JSON.parse(localStorage.getItem(itemKey));
 
-    refs.list.insertAdjacentHTML("beforeend", markupItem(taskObj));
+    taskList.push(newTask);
+    localStorage.setItem(itemKey, JSON.stringify(taskList));
+
+
+        
+    refs.list.insertAdjacentHTML("beforeend", markupItem(newTask));
     refs.form.reset(); 
-});
-
-// form.addEventListener('submit', (event) => {
-//   event.preventDefault();
-//   const taskName = event.target.elements.taskName.value;
-//   const taskDescription = event.target.elements.taskDescription.value;
-
-//   const newTask = {
-//     name: taskName,
-//     description: taskDescription,
-//   };
-//   console.log(newTask);
-// })
+})
 
 
+refs.list.addEventListener("click", (ev) => {
+    
+    if (!ev.target.classList.contains("task-list-item-btn")) {
+        return;
+    }
 
+    const perrentLi = ev.target.closest(".task-list-item");
+    const id = perrentLi.dataset.id;
+
+    if (perrentLi) {
+        perrentLi.remove();
+
+        const taskList = JSON.parse(localStorage.getItem(itemKey));
+
+        const newTaskList = taskList.filter(item => item.id !== id);
+        localStorage.setItem(itemKey, JSON.stringify(newTaskList));
+
+    }
+
+
+})
 
